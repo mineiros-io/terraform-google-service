e199489ce74fdce846d01e558c15541b0941bcb1
samen93 <samen3393@gmail.com>
2021-11-03 11:48:34 +0200

    fix: changing vars default values

--- variables.tf
+++ variables.tf
@@ -22,13 +22,13 @@ variable "project" {
 variable "disable_dependent_services" {
   description = "(Optional) If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed. If false or unset, an error will be generated if any enabled services depend on this service when destroying it."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "disable_on_destroy" {
   description = "(Optional) If true, disable the service when the terraform resource is destroyed. Defaults to true. May be useful in the event that a project is long-lived but the infrastructure running in that project changes frequently."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "timeouts" {
